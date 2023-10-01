c2fa5c9b76ea88032ab5e3cd28c51d950d0eead2
xiayudev <jctuesta7@gmail.com>
2023-09-30 19:29:30 -0500

    feat: load data from frontend

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,6 +1,6 @@
 $(() => {
   $('input').css({'margin-right': '10px'}); // Set margin for all inputs
-  let amenities = []; // Store all checked amenities
+  let amenities = {}; // Store all checked amenities
 
   /**
    * Listen if an input is checked. If so, store it in amenities
@@ -8,13 +8,10 @@ $(() => {
    */
   $("input:checkbox").on('change', function () {
     if ($( this ).prop('checked')){
-      amenities.push($( this ).data('name'));
+      amenities[$( this ).data('id')] = $( this ).data('name');
     } else {
-      const search = $( this ).data('name');
-      amenities = amenities.filter(function(amenity) {
-        return amenity !== search;
-      });
+      delete amenities[$(this).data('id')];
     }
-    $('.amenities h4').text(amenities.join(', ')); // Join all checked amenities
+    $('.amenities h4').text(Object.values(amenities).join(', ')); // Join all checked amenities
   });
 });
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,8 +1,24 @@
 $(() => {
+  $('input').css({'margin-right': '10px'}); // Set margin for all inputs
+  let amenities = {}; // Store all checked amenities
+
+  /**
+   * Listen if an input is checked. If so, store it in amenities
+   * If the input is unchecked by the user, make a filter to delete it
+   */
+  $("input:checkbox").on('change', function () {
+    if ($( this ).prop('checked')){
+      amenities[$( this ).data('id')] = $( this ).data('name');
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    $('.amenities h4').text(Object.values(amenities).join(', ')); // Join all checked amenities
+  });
+  
   $.ajax({
     type: "GET",
     url: "http://127.0.0.1:5001/api/v1/status/",
-    dataType: "JSON"
+    dataType: "json"
   }).done((data) => {
     if (data.status === 'OK')
       $("div#api_status").addClass('available');
